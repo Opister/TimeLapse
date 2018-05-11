4c152852049cad9795fc397ede7308b3cf975ce4
Dominik <Dominik@141.58.60.129>
2018-05-11 16:23:46 +0200

    Implemented Login

--- Ptyp2/src/data/sql/DataBaseController.java
+++ Ptyp2/src/data/sql/DataBaseController.java
@@ -3,12 +3,18 @@ import java.sql.*;
 
 public class DataBaseController {
 	
-	public static void DBTest() {
+	public static Connection DBTest() {
 		try {
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/time_input?autoReconnect=true&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","root");
-		
+		return con;
 		}catch(Exception exc) {
 			exc.printStackTrace();
 		}
+		return null;
+		
 	}
+	
+	
+	
+	
 }
---
+++ Ptyp2/src/data/sql/Login.java
@@ -0,0 +1,41 @@
+package data.sql;
+import java.sql.*;
+
+public class Login {
+	
+	public static boolean checkID(String id) {
+		try {
+			Connection con = DataBaseController.DBTest();
+			String sql = "SELECT 1 FROM time_input.employee WHERE ID = " + id;
+			
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			System.out.println(rs);
+			return rs.next();
+			
+			}catch(Exception exc) {
+				exc.printStackTrace();
+			}
+		return false;
+	}
+	public static String userLogin(String id, String password) {
+		if(Login.checkID(id)) {
+			try {
+			String sql = "SELECT * FROM time_input.employee WHERE Passwort = '"+ password +"' AND ID = " + id;
+			System.out.println(sql);
+			Connection con = DataBaseController.DBTest();
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			if(rs.next()) {
+			System.out.println("Login_erfolgreich");
+			return id;
+			}
+			
+			}catch(Exception exc) {
+				exc.printStackTrace();
+			}
+		}
+		System.out.println("Login_fehgeschlagen");
+		return null;
+	}
+}
