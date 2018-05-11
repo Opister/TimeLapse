22145b9dcd05da6236638f31bf6602941a10c4b5
marmants <marmants@gspc36.informatik.uni-stuttgart.de>
2018-05-11 14:42:11 +0200

    Protocol View

---
+++ Ptyp2/src/application/view/ProtocolViewController.java
@@ -0,0 +1,12 @@
+package application.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class ProtocolViewController {
+	@FXML public TextArea protocolTextArea;
+
+	@FXML public Button backButton;
+
+}
